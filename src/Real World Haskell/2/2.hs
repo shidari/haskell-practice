a941ea454b7ba8064984991e8249cf4cf4b29208
shidari <s.hidari@icloud.com>
2025-12-16 17:15:18 +0900

    answer

--- src/Real World Haskell/2/2.hs
+++ src/Real World Haskell/2/2.hs
@@ -4,6 +4,6 @@ lastButOne [_] = error "List is too short"
 lastButOne [a,_] = a 
 lastButOne (_:xs) = lastButOne xs
 
-main :: IO ()
-main = do
-    print $ lastButOne [1,2,3,4]
+-- main :: IO ()
+-- main = do
+--     print $ lastButOne [1,2,3,4]
